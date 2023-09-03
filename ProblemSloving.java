e5bc98842109ff713216e612c32357c26c55aeff
Mahmoud Samy <mahmoudsamypk@gmail.com>
2023-09-03 07:14:39 +0300

    sec commit

--- ProblemSloving.java
+++ ProblemSloving.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class ProblemSloving {
 
     public static void main(String[] args) {
-//        watermelon problem lin https://codeforces.com/problemset/problem/4/A
+//        watermelon problem link https://codeforces.com/problemset/problem/4/A
         Scanner scan = new Scanner(System.in);
         int numberValue = scan.nextInt();
         System.out.println((numberValue == 2 || numberValue % 2 != 0) ? "NO" : "YES");
